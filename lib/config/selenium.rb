8278f2c49dae835cf32bce1e3582cf783c2c1f23
David Ellis <david.ellis@timgroup.com>
2012-07-25 20:58:04 +0100

    dellis: adding selenium config rb

--- lib/config/selenium.rb
+++ lib/config/selenium.rb
@@ -1,10 +1,18 @@
 
 define "selenium" do
   ubuntuprecise
+
+  run("create ci user") {
+    chroot "/usr/sbin/useradd ci"
+  }
+
   run("install selenium packages") {
     apt_install "google-chrome-stable"
+    apt_install "firefox"
     apt_install "selenium"
     apt_install "selenium-node"
+    apt_install "openjdk-6-jre"
+    apt_install "xvfb"
     chroot "update-rc.d selenium-node defaults"
     chroot "sed -i'.bak' -e 's#^securerandom.source=file:/dev/urandom#securerandom.source=file:/dev/../dev/urandom#g' /etc/java-6-openjdk/security/java.security"
   }
