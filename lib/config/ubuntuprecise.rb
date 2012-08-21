27a66bb6797c5e71b4a561bee96959dd91ad030c
dellis <david.ellis@timgroup.com>
2012-08-21 19:15:09 +0100

    dellis: merged with latest correct version of ubuntuprecise.rb

--- lib/config/ubuntuprecise.rb
+++ lib/config/ubuntuprecise.rb
@@ -1,6 +1,5 @@
 require 'provision/image/catalogue'
 require 'provision/image/commands'
-require 'yaml'
 
 define "ubuntuprecise" do
   extend Provision::Image::Commands
@@ -16,24 +15,24 @@ define "ubuntuprecise" do
   }
 
   cleanup {
-    while(`dmsetup ls | grep #{spec[:loop0]}p1 | wc -l`.chomp != "0")
-      cmd "kpartx -d /dev/#{spec[:loop0]}"
-      sleep(0.1)
-    end
-    cmd "udevadm settle"
+   while(`dmsetup ls | grep #{spec[:loop0]}p1 | wc -l`.chomp != "0")
+     cmd "kpartx -d /dev/#{spec[:loop0]}"
+     sleep(0.1)
+   end
+   cmd "udevadm settle"
 
-    while(`losetup -a | grep /dev/#{spec[:loop0]} | wc -l`.chomp != "0")
-      cmd "losetup -d /dev/#{spec[:loop0]}"
-      sleep(0.1)
-    end
+   while(`losetup -a | grep /dev/#{spec[:loop0]} | wc -l`.chomp != "0")
+     cmd "losetup -d /dev/#{spec[:loop0]}"
+     sleep(0.1)
+   end
 
-    while (`ls -d  #{spec[:temp_dir]} 2> /dev/null | wc -l`.chomp != "0")
-      cmd "umount #{spec[:temp_dir]}"
-      cmd "rmdir #{spec[:temp_dir]}"
-      sleep(0.1)
-    end
-    cmd "udevadm settl"
-  }
+   while (`ls -d  #{spec[:temp_dir]} 2> /dev/null | wc -l`.chomp != "0")
+     cmd "umount #{spec[:temp_dir]}"
+     cmd "rmdir #{spec[:temp_dir]}"
+     sleep(0.1)
+   end
+   cmd "udevadm settle"
+ }
 
   run("loopback devices 2") {
     cmd "losetup /dev/#{spec[:loop1]} /dev/mapper/#{spec[:loop0]}p1"
@@ -41,18 +40,20 @@ define "ubuntuprecise" do
   }
 
   cleanup {
-    while(`losetup -a | grep /dev/#{spec[:loop1]} | wc -l`.chomp != "0")
-      cmd "umount -d /dev/#{spec[:loop1]}"
-      cmd "losetup -d /dev/#{spec[:loop1]}"
+   while(`losetup -a | grep /dev/#{spec[:loop1]} | wc -l`.chomp != "0")
+     cmd "umount -d /dev/#{spec[:loop1]}"
+     cmd "losetup -d /dev/#{spec[:loop1]}"
     end
   }
 
   run("running debootstrap") {
-    #    cmd "debootstrap --arch amd64 precise #{spec[:temp_dir]} http://aptproxy:3142/ubuntu"
+#    cmd "debootstrap --arch amd64 precise #{spec[:temp_dir]} http://aptproxy:3142/ubuntu"
     cmd "mkdir #{spec[:temp_dir]}/proc"
     cmd "mkdir #{spec[:temp_dir]}/sys"
     cmd "mkdir #{spec[:temp_dir]}/dev"
+    cmd "mkdir -p #{spec[:temp_dir]}/etc/default"
   }
+
 
   run("mounting devices") {
     cmd "mount --bind /dev #{spec[:temp_dir]}/dev"
@@ -64,19 +65,127 @@ define "ubuntuprecise" do
     # FIXME Remove the sleep from here, ideally before dellis sees and stabs me.
     # Sleep required because prior steps do not release their file handles quick enough - or something.
 
-    while(`mount -l | grep #{spec[:temp_dir]}/proc | wc -l`.chomp != "0")
-      cmd "umount #{spec[:temp_dir]}/proc"
-      sleep(0.5)
-    end
 
-    while (`mount -l | grep #{spec[:temp_dir]}/sys | wc -l`.chomp != "0")
-      cmd "umount #{spec[:temp_dir]}/sys"
+   while(`mount -l | grep #{spec[:temp_dir]}/proc | wc -l`.chomp != "0")
+       cmd "umount #{spec[:temp_dir]}/proc"
       sleep(0.5)
-    end
+   end
 
-    while( `mount -l | grep #{spec[:temp_dir]}/dev | wc -l`.chomp != "0")
+
+   while (`mount -l | grep #{spec[:temp_dir]}/sys | wc -l`.chomp != "0")
+     cmd "umount #{spec[:temp_dir]}/sys"
+     sleep(0.5)
+   end
+
+   while( `mount -l | grep #{spec[:temp_dir]}/dev | wc -l`.chomp != "0")
       cmd "umount #{spec[:temp_dir]}/dev"
       sleep(0.5)
-    end
+   end
   }
+  run("set locale") {
+    open("#{spec[:temp_dir]}/etc/default/locale", 'w') { |f|
+      f.puts 'LANG="en_GB.UTF-8"'
+    }
+
+    chroot "locale-gen en_GB.UTF-8"
+  }
+
+  run("set timezone") {
+    open("#{spec[:temp_dir]}/etc/timezone", 'w') { |f|
+      f.puts 'Europe/London'
+    }
+
+    chroot "dpkg-reconfigure --frontend noninteractive tzdata"
+  }
+
+  run("set hostname") {
+    open("#{spec[:temp_dir]}/etc/hostname", 'w') { |f|
+      f.puts "#{hostname}"
+    }
+    chroot "hostname -F /etc/hostname"
+  }
+
+  run("install kernel and grub") {
+    chroot "apt-get -y --force-yes update"
+    apt_install "linux-image-virtual"
+    apt_install "grub-pc"
+
+    cmd "mkdir -p #{spec[:temp_dir]}/boot/grub"
+
+    open("#{spec[:temp_dir]}/boot/grub/device.map", 'w') { |f|
+      f.puts "(hd0) /dev/#{spec[:loop0]}"
+      f.puts "(hd0,1) /dev/#{spec[:loop1]}"
+    }
+
+    kernel_version = "3.2.0-23-virtual"
+    kernel = "/boot/vmlinuz-#{kernel_version}"
+    initrd = "/boot/initrd.img-#{kernel_version}"
+    uuid = `blkid -o value /dev/mapper/#{spec[:loop0]}p1 | head -n1`.chomp
+
+    open("#{spec[:temp_dir]}/boot/grub/grub.cfg", 'w') { |f|
+      f.puts "
+          set default=\"0\"
+          set timeout=1
+          menuentry 'Ubuntu, with Linux #{kernel_version}' --class ubuntu --class gnu-linux --class gnu --class os {
+          insmod part_msdos
+          insmod ext2
+          set root='(hd0,1)'
+          linux #{kernel} root=/dev/disk/by-uuid/#{uuid} ro
+          initrd #{initrd}
+          }"
+    }
+
+    chroot "grub-install --no-floppy --grub-mkdevicemap=/boot/grub/device.map /dev/#{spec[:loop0]}"
+  }
+
+  run("set root password") {
+    chroot "echo 'root:root' | chpasswd"
+  }
+
+  run("set up basic networking") {
+    open("#{spec[:temp_dir]}/etc/network/interfaces", 'w') { |f|
+      f.puts "
+     # The loopback network interface
+     auto lo
+     iface lo inet loopback
+     # The primary network interface
+     auto eth0
+     iface eth0 inet dhcp
+       "
+    }
+  }
+
+  run("install misc packages") {
+    apt_install "acpid openssh-server curl vim"
+  }
+
+  # A few daemons hang around at the end of the bootstrapping process that prevent us unmounting.
+  cleanup {
+    chroot "/etc/init.d/acpid stop"
+    chroot "/etc/init.d/cron stop"
+  }
+
+  run("configure precise repos apt repo") {
+    open("#{spec[:temp_dir]}/etc/apt/sources.list.d/ubuntu-precise.list", 'w') { |f|
+      f.puts "deb http://aptproxy:3142/ubuntu precise-updates main\n"
+    }
+  }
+
+  run("configure youdevise apt repo") {
+    open("#{spec[:temp_dir]}/etc/apt/sources.list.d/youdevise.list", 'w') { |f|
+      f.puts "deb http://apt/ubuntu stable main\ndeb-src http://apt/ubuntu stable main\n"
+    }
+
+    chroot "curl -Ss http://apt/ubuntu/repo.key | apt-key add -"
+  }
+
+  run("configure google apt repo") {
+    open("#{spec[:temp_dir]}/etc/apt/sources.list.d/google.list", 'w') { |f|
+      f.puts "deb http://dl.google.com/linux/deb/ stable main\n"
+    }
+
+    chroot "curl -Ss https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -"
+    chroot "apt-get update"
+  }
+
 end
