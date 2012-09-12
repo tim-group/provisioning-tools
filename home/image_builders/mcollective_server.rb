bc6c5467ab51348efeb8914b416bb27db56f490e
dellis <david.ellis@timgroup.com>
2012-09-12 12:29:00 +0100

    sorting out activemq

--- home/image_builders/mcollective_server.rb
+++ home/image_builders/mcollective_server.rb
@@ -6,6 +6,7 @@ define "mcollective_server" do
 
     chroot "ln -s /opt/activemq/bin/activemq /etc/init.d/activemq"
     cmd "cp #{Dir.pwd}/files/activemq.xml #{spec[:temp_dir]}/opt/activemq/conf/"
+    chroot "update-rc.d activemq defaults"
   }	
 
 end
