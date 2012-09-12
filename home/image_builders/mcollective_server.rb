a79dd895885be7c4f188aa2f51188f25a8f33b0a
dellis <david.ellis@timgroup.com>
2012-09-12 11:52:52 +0100

    sorting out dhcp config

--- home/image_builders/mcollective_server.rb
+++ home/image_builders/mcollective_server.rb
@@ -3,7 +3,7 @@ define "mcollective_server" do
 
   run("install and configure activemq") {
     apt_install "activemq"
-    log.debug("CWD #{Dir.pwd}")
+    log.info("CWD #{Dir.pwd}")
 
     chroot "ln -s /opt/activemq/bin/activemq /etc/init.d/activemq"
     cmd "cp #{spec[:temp_dir]}/../../files/activemq.xml /opt/activemq/conf/"
