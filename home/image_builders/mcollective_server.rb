d41ab48ddd0d7bdaebcf8e856732345500d59fe4
David Ellis <david.ellis@timgroup.com>
2012-09-12 17:50:29 +0100

    setting basic mcollective stuff

--- home/image_builders/mcollective_server.rb
+++ home/image_builders/mcollective_server.rb
@@ -7,6 +7,17 @@ define "mcollective_server" do
     chroot "ln -s /opt/activemq/bin/activemq /etc/init.d/activemq"
     cmd "cp #{Dir.pwd}/files/activemq.xml #{spec[:temp_dir]}/opt/activemq/conf/"
     chroot "update-rc.d activemq defaults"
-  }	
+  }
+
+  run("install and configure mcollective") {
+    apt_install "ruby-stomp"
+    apt_install "mcollective"
+    cmd "cp #{Dir.pwd}/files/mcollective/server.cfg #{spec[:temp_dir]}/opt/activemq/conf/"
+  }
+
+  run("install and configure mcollective client") {
+    apt_install "mcollective-client"
+    cmd "cp #{Dir.pwd}/files/mcollective/client.cfg #{spec[:temp_dir]}/opt/activemq/conf/"
+  }
 
 end
