cb42275d8bfed0cc9cce7403f7c2f3b8af7ce83b
Adam Gibbins <adam.gibbins@timgroup.com>
2012-11-01 15:58:43 -0400

    Continue even if stuff fails

--- home/image_builders/puppetmaster.rb
+++ home/image_builders/puppetmaster.rb
@@ -88,7 +88,7 @@ port   = 8081
 DEBIAN_FRONTEND=noninteractive apt-get -y --force-yes install puppetdb
 update-rc.d puppetdb defaults
 # FIXME Grep the log to check its started then kill it instead?
-puppet master --verbose --logdest=syslog && sleep 5 && killall puppet
+puppet master --verbose --logdest=syslog; sleep 5; killall puppet
 echo \"#!/bin/sh -e\nexit 0\" > /etc/rc.local
 exit 0
 """
