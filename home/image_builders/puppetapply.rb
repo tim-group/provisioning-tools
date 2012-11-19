87e6c50ec2b7912b8db3dab40a16e709064b55a5
Richard Pearce <richard.pearce@timgroup.com>
2012-11-19 17:40:17 +0000

    rpearce/grahamc: Allowed use by individual seeds.

--- home/image_builders/puppetapply.rb
+++ home/image_builders/puppetapply.rb
@@ -2,12 +2,13 @@ define "puppetapply" do
   ubuntuprecise
 
   run("puppetapply") {
-    cmd "cp -r #{File.dirname(__FILE__)}/seed/#{spec[:seed]}  #{spec[:temp_dir]}/seed"
+    cmd "mkdir #{spec[:temp_dir]}/seed"
+    cmd "cp -r #{File.dirname(__FILE__)}/seed/#{spec[:seed]}/  #{spec[:temp_dir]}/seed"
     apt_install "puppet"
 
     open("#{spec[:temp_dir]}/etc/rc.local", 'w') { |f|
       f.puts """#!/bin/sh -e
- puppet apply /seed/install.pp -l /seed/init.log
+ puppet apply /seed/#{spec[:seed]}/install.pp --modulepath=/seed/#{spec[:seed]} -l /seed/init.log
  echo \"#!/bin/sh -e\nexit 0\" > /etc/rc.local
  exit 0
       """
