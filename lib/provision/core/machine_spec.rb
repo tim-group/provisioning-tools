a98fec5ac139a3d813c19cbd774337fe671609c9
Tomas Doran <tomas.doran@timgroup.com>
2012-12-03 21:21:23 +0000

    Fix log locations

--- lib/provision/core/machine_spec.rb
+++ lib/provision/core/machine_spec.rb
@@ -55,7 +55,7 @@ class Provision::Core::MachineSpec
   end
 
   def get_logger(fn)
-    Logger.new("#{@log_dir}/%{fn}-#{@thread_number}.log")
+    Logger.new("#{@log_dir}/#{fn}-#{@thread_number}.log")
   end
 end
 
