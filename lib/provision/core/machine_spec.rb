e145d0c6a8aae0d7872ba61a390bd516f2f9a197
Tomas Doran <tomas.doran@timgroup.com>
2012-12-03 20:54:08 +0000

    Remove duplicate info

--- lib/provision/core/machine_spec.rb
+++ lib/provision/core/machine_spec.rb
@@ -10,14 +10,14 @@ class Provision::Core::MachineSpec
 
   def initialize(spec)
     @thread_number = spec[:thread_number] || 0
-    @build_dir = spec[:build_dir] || 'build'
+    b_d = spec[:build_dir] || 'build'
+    @build_dir = "#{Provision.base()}/#{b_d}"
     @spec = spec
     apply_conventions()
   end
 
   def apply_conventions()
     if_nil_define_var(:thread_number, 0)
-    if_nil_define_var(:build_dir, "build")
     if_nil_define_var(:images_dir,"#{@spec[:spindle]}")
     if_nil_define_var(:image_path,"#{@spec[:images_dir]}/#{@spec[:hostname]}.img")
     if_nil_define_var(:image_size,"3G")
@@ -25,9 +25,9 @@ class Provision::Core::MachineSpec
     if_nil_define_var(:loop0,"loop#{@thread_number*2}")
     if_nil_define_var(:loop1,"loop#{@thread_number*2+1}")
 
-    if_nil_define_var(:logdir,"#{Provision.home()}/../#{@build_dir}/logs")
-    if_nil_define_var(:console_log,"#{Provision.base()}/#{@build_dir}/logs/console-#{@thread_number}.log")
-    if_nil_define_var(:temp_dir, "#{Provision.base()}/#{@build_dir}/#{@spec[:hostname]}")
+    if_nil_define_var(:logdir,"#{@build_dir}/logs")
+    if_nil_define_var(:console_log,"#{@build_dir}/logs/console-#{@thread_number}.log")
+    if_nil_define_var(:temp_dir, "#{@build_dir}/#{@spec[:hostname]}")
 
     if_nil_define_var(:fqdn,"#{@spec[:hostname]}.#{@spec[:domain]}")
 
