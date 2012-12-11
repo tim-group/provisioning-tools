cc95f8d9e9b4f4fd37394a46ea728273774772b8
Tomas Doran <tomas.doran@timgroup.com>
2012-12-11 16:14:54 +0000

    Very very basic but seemingly working IP allocator

--- lib/provision/dns.rb
+++ lib/provision/dns.rb
@@ -2,7 +2,8 @@ class Provision::DNS
   def self.get_backend(name)
     require "provision/dns/#{name.downcase}"
     classname = "Provision::DNS::#{name}"
-    classname.new()
+    puts "Loaded provision/dns/#{name.downcase} construct #{classname}"
+    Provision::DNS.const_get(name).new()
   end
 end
 
--- lib/provision/dns/dnsmasq.rb
+++ lib/provision/dns/dnsmasq.rb
@@ -1,11 +1,49 @@
+require "yaml"
 class Provision::DNS::DNSMasq < Provision::DNS
+  @@hosts_file = "/etc/hosts"
+  @@ethers_file = "/etc/ethers"
   def initialize()
     super
+    parse_hosts
   end
 
   def allocate_ip_for(spec)
-    "1.1.1.1"
+    hn = spec[:fqdn]
+    puts "ALLOCATE IP FOR #{spec.to_yaml}"
+    if @by_name[hn]
+      puts "No new allocation for #{hn}, already allocated to #{@by_name[hn]}"
+      @by_name[hn]
+    else
+      # FIXME - THERE IS NO CHECKING HERE - THIS WILL ALLOCATE THE BROADCAST ADDRESS...
+      @max_ip = IPAddr.new(@max_ip.to_i + 1, Socket::AF_INET)
+      puts "Allocated IP #{@max_ip} to host #{hn}"
+      File.open(@@hosts_file, 'a') { |f| f.write "#{@max_ip.to_s} #{hn}\n" }
+      File.open(@@ethers_file, 'a') { |f| f.write "#{spec.interfaces[0][:mac]} #{@max_ip.to_s}\n" }
+      @max_ip
+    end
   end
 
+  def parse_hosts
+    require 'ipaddr'
+    network = IPAddr.new("192.168.5.0/24")
+    @by_name = {}
+    @max_ip = IPAddr.new("192.168.5.1")
+
+    File.open(@@hosts_file).each { |l|
+      next if l =~ /^#/
+      next if l =~ /^\s*$/
+      next unless l =~ /^\d+\.\d+\.\d+\.\d+/
+      splits = l.split("\s")
+      ip = splits[0]
+      names = splits[1..-1]
+      next unless network.include?(ip)
+      if IPAddr.new(ip) > @max_ip
+        @max_ip = IPAddr.new(ip)
+      end
+      names.each { |n|
+         @by_name[n] = ip
+      }
+    }
+  end
 end
 
