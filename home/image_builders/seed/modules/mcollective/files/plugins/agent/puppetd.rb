f83f12ad06f7ed3d7364886ae13b65cc4e16d3b0
Richard Pearce <richard.pearce@timgroup.com>
2012-12-12 16:34:01 +0000

    rpearce/tanderson: Modified mcollective agent to clean-up stale pid files

--- home/image_builders/seed/modules/mcollective/files/plugins/agent/puppetd.rb
+++ home/image_builders/seed/modules/mcollective/files/plugins/agent/puppetd.rb
@@ -10,27 +10,20 @@ module MCollective
     #    puppetd.lockfile  - Where to find the lock file; defaults to
     #                        /var/lib/puppet/state/puppetdlock
     #    puppetd.puppetd   - Where to find the puppet agent binary; defaults to
-    #                        /usr/sbin/puppetd
+    #                        /usr/bin/puppet agent
     #    puppetd.summary   - Where to find the summary file written by Puppet
     #                        2.6.8 and newer; defaults to
     #                        /var/lib/puppet/state/last_run_summary.yaml
     #    puppetd.pidfile   - Where to find puppet agent's pid file; defaults to
     #                        /var/run/puppet/agent.pid
     class Puppetd<RPC::Agent
-      metadata    :name        => "Puppet Controller Agent",
-                  :description => "Run puppet agent, get its status, and enable/disable it",
-                  :author      => "R.I.Pienaar",
-                  :license     => "Apache License 2.0",
-                  :version     => "1.6",
-                  :url         => "http://projects.puppetlabs.com/projects/mcollective-plugins/wiki/AgentPuppetd",
-                  :timeout     => 30
 
       def startup_hook
         @splaytime = @config.pluginconf["puppetd.splaytime"].to_i || 0
         @lockfile = @config.pluginconf["puppetd.lockfile"] || "/var/lib/puppet/state/puppetdlock"
         @statefile = @config.pluginconf["puppetd.statefile"] || "/var/lib/puppet/state/state.yaml"
         @pidfile = @config.pluginconf["puppet.pidfile"] || "/var/run/puppet/agent.pid"
-        @puppetd = @config.pluginconf["puppetd.puppetd"] || "/usr/sbin/puppetd"
+        @puppetd = @config.pluginconf["puppetd.puppetd"] || "/usr/bin/puppet agent"
         @last_summary = @config.pluginconf["puppet.summary"] || "/var/lib/puppet/state/last_run_summary.yaml"
       end
 
@@ -79,12 +72,27 @@ module MCollective
       def puppet_daemon_status
         locked = File.exists?(@lockfile)
         disabled = locked && File::Stat.new(@lockfile).zero?
-        has_pid = File.exists?(@pidfile)
+        has_pid = check_pid_running
 
         return 'disabled' if disabled
         return 'running'  if   locked && has_pid
         return 'idling'   if ! locked && has_pid
         return 'stopped'  if ! has_pid
+      end
+
+      def check_pid_running
+        if (File.exists?(@pidfile))
+          pid = File.read(@pidfile)
+          if pid =~ /^\d+$/
+            begin
+              ::Process.kill(0, Integer(pid)) # check that pid is alive
+              return true
+            rescue Errno::ESRCH => e
+            end
+          end
+          FileUtils.rm(@pidfile)
+        end
+        return false
       end
 
       def runonce
@@ -98,25 +106,14 @@ module MCollective
 
         when 'idling' then       # signal daemon
           pid = File.read(@pidfile)
-          if pid !~ /^\d+$/
-            reply.fail "PID file does not contain a PID; got #{pid.inspect}"
-          else
-            begin
-              ::Process.kill(0, Integer(pid)) # check that pid is alive
-              # REVISIT: Should we add an extra round of security here, and
-              # ensure that the PID file is securely owned, or that the target
-              # process looks like Puppet?  Otherwise a malicious user could
-              # theoretically signal arbitrary processes with this...
-              begin
-                ::Process.kill("USR1", Integer(pid))
-                reply[:output] = "Signalled daemonized puppet agent to run (process #{Integer(pid)}); " + (reply[:output] || '')
-              rescue Exception => e
-                reply.fail "Failed to signal the puppet agent daemon (process #{pid}): #{e}"
-              end
-            rescue Errno::ESRCH => e
-              # PID is invalid, run puppet onetime as usual
-              runonce_background
-            end
+          begin
+            ::Process.kill("USR1", Integer(pid))
+            reply[:output] = "Signalled daemonized puppet agent to run (process #{Integer(pid)}); " + (reply[:output] || '')
+          rescue Exception => e
+            reply.fail "Failed to signal the puppet agent daemon (process #{pid}): #{e}"
+          rescue Errno::ESRCH => e
+            set_status
+            runonce_background
           end
 
         when 'stopped' then      # just run
