280af37659fb695c712f77b42e6058036829ea72
Richard Pearce <richard.pearce@timgroup.com>
2013-01-10 17:06:05 +0000

    rpearce/tanderson: Added clean method, re-jigged stuff

--- mcollective/agent/computenode.rb
+++ mcollective/agent/computenode.rb
@@ -4,23 +4,43 @@ module MCollective
   module Agent
     class Computenode < RPC::Agent
 
-      def provision(specs)
+      def prepare_work_queue(specs, listener)
         require 'provision'
         require 'provision/inventory'
         require 'provision/workqueue'
-        listener = NoopListener.new()
         work_queue = Provision.work_queue(:worker_count=>1, :listener=>listener)
         work_queue.fill(specs)
-        work_queue.process()
-        return listener.results()
+        end
+      end
+
+      def with_lock(&action)
+        File.open("/var/lock/provision.lock", "w") do |f|
+          f.flock(File::LOCK_EX)
+          action.call
+        end
       end
 
       action "launch" do
-        File.open("/var/lock/provision.lock", "w") do |f|
-          f.flock(File::LOCK_EX)
+        with_lock do
           specs = request[:specs]
-          puts "Building #{specs.size} nodes"
-          reply.data = provision(specs)
+          listener = NoopListener.new
+          queue = prepare_work_queue(specs, listener)
+
+          puts "Launching #{specs.size} nodes"
+          reply.data = queue.process(specs)
+          return listener.results
+        end
+      end
+
+      action "clean" do
+        with_lock do
+          specs = request[:specs]
+          listener = NoopListener.new
+          queue = prepare_work_queue(specs, listener)
+
+          puts "Cleaning #{specs.size} nodes"
+          reply.data = queue.clean(specs)
+          return listener.results
         end
       end
     end
