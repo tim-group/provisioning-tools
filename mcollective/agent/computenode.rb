0c0f3eb227b0327e8d73ecc0ff16e07e7e5169df
Richard Pearce <richard.pearce@timgroup.com>
2013-01-10 17:12:20 +0000

    End fudgification sorry

--- mcollective/agent/computenode.rb
+++ mcollective/agent/computenode.rb
@@ -11,36 +11,36 @@ module MCollective
         work_queue = Provision.work_queue(:worker_count=>1, :listener=>listener)
         work_queue.fill(specs)
       end
-    end
 
-    def with_lock(&action)
-      File.open("/var/lock/provision.lock", "w") do |f|
-        f.flock(File::LOCK_EX)
-        action.call
+      def with_lock(&action)
+        File.open("/var/lock/provision.lock", "w") do |f|
+          f.flock(File::LOCK_EX)
+          action.call
+        end
       end
-    end
 
-    action "launch" do
-      with_lock do
-        specs = request[:specs]
-        listener = NoopListener.new
-        queue = prepare_work_queue(specs, listener)
+      action "launch" do
+        with_lock do
+          specs = request[:specs]
+          listener = NoopListener.new
+          queue = prepare_work_queue(specs, listener)
 
-        puts "Launching #{specs.size} nodes"
-        reply.data = queue.process(specs)
-        return listener.results
+          puts "Launching #{specs.size} nodes"
+          reply.data = queue.process(specs)
+          return listener.results
+        end
       end
-    end
 
-    action "clean" do
-      with_lock do
-        specs = request[:specs]
-        listener = NoopListener.new
-        queue = prepare_work_queue(specs, listener)
+      action "clean" do
+        with_lock do
+          specs = request[:specs]
+          listener = NoopListener.new
+          queue = prepare_work_queue(specs, listener)
 
-        puts "Cleaning #{specs.size} nodes"
-        reply.data = queue.clean(specs)
-        return listener.results
+          puts "Cleaning #{specs.size} nodes"
+          reply.data = queue.clean(specs)
+          return listener.results
+        end
       end
     end
   end
