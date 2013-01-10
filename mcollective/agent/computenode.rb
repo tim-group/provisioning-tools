0a5997b499cab3bb13651c560cad30da7a621916
Richard Pearce <richard.pearce@timgroup.com>
2013-01-10 17:10:40 +0000

    Ooops spurious additional end removed

--- mcollective/agent/computenode.rb
+++ mcollective/agent/computenode.rb
@@ -10,38 +10,37 @@ module MCollective
         require 'provision/workqueue'
         work_queue = Provision.work_queue(:worker_count=>1, :listener=>listener)
         work_queue.fill(specs)
-        end
       end
+    end
 
-      def with_lock(&action)
-        File.open("/var/lock/provision.lock", "w") do |f|
-          f.flock(File::LOCK_EX)
-          action.call
-        end
+    def with_lock(&action)
+      File.open("/var/lock/provision.lock", "w") do |f|
+        f.flock(File::LOCK_EX)
+        action.call
       end
+    end
 
-      action "launch" do
-        with_lock do
-          specs = request[:specs]
-          listener = NoopListener.new
-          queue = prepare_work_queue(specs, listener)
+    action "launch" do
+      with_lock do
+        specs = request[:specs]
+        listener = NoopListener.new
+        queue = prepare_work_queue(specs, listener)
 
-          puts "Launching #{specs.size} nodes"
-          reply.data = queue.process(specs)
-          return listener.results
-        end
+        puts "Launching #{specs.size} nodes"
+        reply.data = queue.process(specs)
+        return listener.results
       end
+    end
 
-      action "clean" do
-        with_lock do
-          specs = request[:specs]
-          listener = NoopListener.new
-          queue = prepare_work_queue(specs, listener)
+    action "clean" do
+      with_lock do
+        specs = request[:specs]
+        listener = NoopListener.new
+        queue = prepare_work_queue(specs, listener)
 
-          puts "Cleaning #{specs.size} nodes"
-          reply.data = queue.clean(specs)
-          return listener.results
-        end
+        puts "Cleaning #{specs.size} nodes"
+        reply.data = queue.clean(specs)
+        return listener.results
       end
     end
   end
