09f558dab023bac1ea9a5e9f76c970040e92ddf6
Tomas Doran <bobtfish@bobtfish.net>
2013-01-14 13:06:00 -0500

    Get simple allocation working

--- lib/provision/dns/ddns.rb
+++ lib/provision/dns/ddns.rb
@@ -1,6 +1,7 @@
 require 'ipaddr'
 require 'rubygems'
 require 'Dnsruby'
+require 'tempfile'
 require 'provision/dns'
 
 class Provision::DNS::DDNS < Provision::DNS
@@ -17,58 +18,97 @@ class Provision::DNS::DDNS < Provision::DNS
     @max_allocation = IPAddr.new(@broadcast.to_i - 1, Socket::AF_INET)
     min_allocation = options[:min_allocation] || 10
     @min_allocation = IPAddr.new(min_allocation.to_i + @network.to_i, Socket::AF_INET)
+    @rndc_key = options[:rndc_key] || raise("No :rndc_key supplied")
   end
 
-  def reverse_zone
-
-    '16.16.172.in-addr.arpa'
+  def write_rndc_key
+    tmp_file = Tempfile.new('remove_temp')
+    tmp_file.puts "key \"rndc-key\" {"
+    tmp_file.puts "algorithm hmac-md5;"
+    tmp_file.puts "secret \"#{@rndc_key}\";"
+    tmp_file.puts "};"
+    tmp_file.close
+    tmp_file
   end
 
   def get_primary_nameserver_for(zone)
-    '172.16.16.5'
-  end
-
-  def send_update(zone, update)
-    res = Dnsruby::Resolver.new({:nameserver => get_primary_nameserver_for(zone)})
-    ok = true
-    begin
-     reply = res.send_message(update)
-     print "Update succeeded\n"
-    rescue Exception => e
-      print "Update failed: #{e}\n"
-      ok = false
-    end
-    ok
+#    '172.16.16.5'
+    '127.0.0.1'
   end
 
   def remove_ips_for(spec)
-    ip = nil
     hn = spec[:fqdn]
-    if @by_name[hn]
-      ip = @by_name[hn]
-      puts "Removing ip allocation (#{ip}) for #{hn}"
-      return true
-    else
-      puts "No ip allocation found for #{hn}, not removing"
-      return false
-    end
+    puts "Not ability to remove DNS for #{hn}, not removing"
+    return false
   end
 
   def try_add_reverse_lookup(ip, fqdn)
-    update = Dnsruby::Update.new(reverse_zone)
     ip_rev = ip.to_s.split('.').reverse.join('.')
-    update.absent("#{ip_rev}.in-addr.arpa.", 'PTR') # prereq
-    update.add("#{ip_rev}.in-addr.arpa.", 'PTR', 86400, "#{fqdn}.")
-    send_update(reverse_zone, update)
+    tmp_file = Tempfile.new('remove_temp')
+    tmp_file.puts "server 127.0.0.1"
+    tmp_file.puts "zone 16.16.172.in-addr.arpa"
+    tmp_file.puts "prereq nxdomain #{ip_rev}.in-addr.arpa"
+    tmp_file.puts "update add #{ip_rev}.in-addr.arpa. 86400 PTR #{fqdn}."
+    tmp_file.puts "send"
+    tmp_file.close
+    out = exec_nsupdate(tmp_file)
+    if out =~ /update failed: YXDOMAIN/
+      puts "FAILED TO ADD #{ip_rev}.in-addr.arpa. PTR #{fqdn}. IP already used"
+      return false
+    else
+      puts "ADD OK for reverse of #{ip} to #{fqdn} => #{out}"
+      return true
+    end
+  end
+
+  def exec_nsupdate(update_file)
+    rndc_tmp = write_rndc_key
+    out = `cat #{update_file.path} | nsupdate -k #{rndc_tmp.path} 2>&1`
+    update_file.unlink
+    rndc_tmp.unlink
+    out
+  end
+
+  def add_forward_lookup(ip, fqdn)
+    fqdn_s = fqdn.split "."
+    zone_s = fqdn_s.clone
+    hn = zone_s.shift
+    zone = zone_s.join('.')
+    tmp_file = Tempfile.new('remove_temp')
+    tmp_file.puts "server 127.0.0.1"
+    tmp_file.puts "zone #{zone}"
+    tmp_file.puts "update add #{fqdn}. 86400 A #{ip}"
+    tmp_file.puts "send"
+    tmp_file.close
+    out = exec_nsupdate(tmp_file)
+    if out =~ /^$/
+      return true
+    else
+      raise("Could not add forward lookup #{fqdn} A #{ip}: #{out}")
+    end
+  end
+
+  def lookup_ip_for(hn)
+    res = Dnsruby::Resolver.new(:nameserver => "127.0.0.1")
+    begin
+      ret = res.query(hn) # Defaults to A record
+      ret.answer.rrset(hn).rrs[0].data
+    rescue Dnsruby::NXDomain
+      puts "Could not find #{hn}"
+      return false
+    rescue Dnsruby::ServFail
+      puts "Could not find #{hn}"
+      return false
+    end
   end
 
   def allocate_ips_for(spec)
     ip = nil
 
     hn = spec[:fqdn]
-    if lookup_ip_for(spec)
-      puts "No new allocation for #{hn}, already allocated to #{@by_name[hn]}"
-      return lookup_ip_for(spec)
+    if lookup_ip_for(hn)
+      puts "No new allocation for #{hn}, already allocated"
+      return lookup_ip_for(hn)
     else
 
       max_ip = @max_allocation
@@ -79,6 +119,7 @@ class Provision::DNS::DDNS < Provision::DNS
             raise("Ran out of ips")
         end
       end
+      add_forward_lookup(ip, hn)
     end
     ip
   end
--- spec/provision/dns/ddns_spec.rb
+++ spec/provision/dns/ddns_spec.rb
@@ -7,13 +7,49 @@ class Provision::DNS::DDNS
   attr_reader :network, :broadcast, :min_allocation, :max_allocation
 end
 
+class MockProvision < Provision::DNS::DDNS
+  attr_reader :update_files
+  def initialize(options={})
+    super
+    @nsupdate_replies = options[:nsupdate_replies] || raise("Need :nsupdate_replies")
+    @lookup_table = options[:lookup_table] || raise("Need :lookup_table")
+    @update_files = []
+  end
+
+  def exec_nsupdate(update_file)
+    @nsupdate_replies.shift
+    @update_files.push(update_file)
+  end
+
+  def lookup_ip_for(hn)
+    @lookup_table[hn] || false
+  end
+end
+
 describe Provision::DNS::DDNS do
   it 'constructs once' do
-    dns = Provision::DNS::DDNS.new(:network_range => '192.168.1.0/24')
+    dns = Provision::DNS::DDNS.new(
+      :network_range => '192.168.1.0/24',
+      :rndc_key      => "fa5dUl+sdm/8cSZtDv1xFw=="
+    )
     expect(dns.network.to_s).to eq('192.168.1.0')
     expect(dns.broadcast.to_s).to eq('192.168.1.255')
     expect(dns.min_allocation.to_s).to eq('192.168.1.10')
     expect(dns.max_allocation.to_s).to eq('192.168.1.254')
   end
+
+  it 'is mocked in subclass as expected' do
+    dns = MockProvision.new(
+      :network_range => '192.168.1.0/24',
+      :rndc_key      => "fa5dUl+sdm/8cSZtDv1xFw==",
+      :nsupdate_replies => [],
+      :lookup_table => {
+        'foo.example.com' => '172.16.0.1',
+      }
+    )
+    expect( dns.lookup_ip_for('foo.example.com') ).to eq('172.16.0.1')
+    expect( dns.lookup_ip_for('foo2example.com') ).to eq(false)
+  end
 end
+
 
