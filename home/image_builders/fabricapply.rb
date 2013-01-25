e24f87e47bc6de289c63ec142c033d3b462d49bf
Richard Pearce <richard.pearce@timgroup.com>
2013-01-25 14:47:52 +0000

    rpearce: Adding puppetmaster foo for fabric

--- home/image_builders/fabricapply.rb
+++ home/image_builders/fabricapply.rb
@@ -4,7 +4,22 @@ define "fabricapply" do
   run("run apt-update ") {
     chroot "apt-get -y --force-yes update"
   }
-
+  run("git clone puppet") {
+    cmd "rm -Rf #{spec[:temp_dir]}/etc/puppet/*"
+    cmd "git clone http://git.youdevise.com/git/puppet #{spec[:temp_dir]}/etc/puppet"
+  }
+  run("temporary route to existing fabric") {
+    open("#{spec[:temp_dir]}//etc/network/if-up.d/routes_mgmt", "w") {|f|
+      f.puts """
+if [ "${IFACE}" == "mgmt" ]; then
+ip route add 10.108.0.0/16 via 172.19.0.3
+fi
+"""
+    }
+    open("#{spec[:temp_dir]}/etc/hosts", 'a') { |f|
+      f.puts "10.108.11.237 aptproxy deb\n"
+    }
+  }
   run("fabricapply") {
     cmd "mkdir #{spec[:temp_dir]}/seed"
     cmd "cp -r #{File.dirname(__FILE__)}/seed  #{spec[:temp_dir]}/"
@@ -15,7 +30,7 @@ define "fabricapply" do
 
     open("#{spec[:temp_dir]}/seed/puppet.sh", 'w') { |f|
       f.puts """#!/bin/sh -e
-puppet apply /seed/manifests/fabric.pp --node_terminus exec --external_nodes /seed/enc.sh --modulepath=/seed/modules 2>&1 | tee /seed/init.log
+puppet apply -v --pluginsync --modulepath=/etc/puppet/modules /etc/puppet/manifests/site.pp 2>&1 | tee /seed/init.log
       """
     }
 
