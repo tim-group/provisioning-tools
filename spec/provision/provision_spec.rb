582100d6cdb0c02f4a967c2d5e198a3b92f100be
Tom Anderson <tom.anderson@timgroup.com>
2013-01-31 12:19:36 +0000

    Make spec which needs a config file read a supplied test config file

--- spec/provision/provision_spec.rb
+++ spec/provision/provision_spec.rb
@@ -2,16 +2,17 @@ require 'rspec'
 require 'provision'
 
 describe Provision::Factory do
+  TEST_CONFIG = File.join(File.dirname(__FILE__), "config.yaml")
+  
   it 'can be constructed' do
-    Provision::Factory.new
+    Provision::Factory.new(:configfile => TEST_CONFIG)
   end
 
   it 'has a working home method' do
-    Provision::Factory.new.home
+    Provision::Factory.new(:configfile => TEST_CONFIG).home
   end
 
   it 'has a working base method' do
-    Provision::Factory.new.base
+    Provision::Factory.new(:configfile => TEST_CONFIG).base
   end
 end
-
