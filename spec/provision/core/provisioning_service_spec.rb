710f9ae38be95e88cf23285ec306a2a82bb8dcc7
Tom Anderson <tom.anderson@timgroup.com>
2013-01-31 16:34:23 +0000

    Bit of whitespace cleanup

--- spec/provision/core/provisioning_service_spec.rb
+++ spec/provision/core/provisioning_service_spec.rb
@@ -15,7 +15,7 @@ module RSpec::Mocks::ArgumentMatchers
 
       def ==(actual)
         return true if actual[:networking] == @expected
-        raise "expecting networking :#{PP.pp(actual.spec[:networking],"")} to match #{PP.pp(@expected,"")}"
+        raise "expecting networking :#{PP.pp(actual.spec[:networking], "")} to match #{PP.pp(@expected, "")}"
       end
 
       def description
@@ -46,40 +46,39 @@ describe Provision::Core::ProvisioningService do
   end
 
   it 'should run the configure sections to define common conventions' do
-
   end
 
   it 'allows the user to define vm from an image catalogue and vmdescription catalogue' do
     @numbering_service.should_receive(:allocate_ips_for)
-    @provisioning_service.should_receive(:clean_vm).with(:hostname=>"vmx1",:template=>"ubuntuprecise")
-    @image_service.should_receive(:build_image).with("ubuntuprecise",anything).ordered
+    @provisioning_service.should_receive(:clean_vm).with(:hostname => "vmx1", :template => "ubuntuprecise")
+    @image_service.should_receive(:build_image).with("ubuntuprecise", anything).ordered
     @vm_service.should_receive(:define_vm).ordered
     @vm_service.should_receive(:start_vm).ordered
-    @provisioning_service.provision_vm(:hostname=>"vmx1",:template=>"ubuntuprecise")
+    @provisioning_service.provision_vm(:hostname => "vmx1", :template => "ubuntuprecise")
   end
 
   it 'allows ips to produce' do
     network_address = {
-      "mgmt"=>{
-        :ip=>"192.168.24.5",
-        :netmask=>"255.255.255.0"
+      "mgmt" => {
+        :ip => "192.168.24.5",
+        :netmask => "255.255.255.0"
       }
     }
     @numbering_service.stub(:allocate_ips_for).and_return(network_address)
 
-    @provisioning_service.should_receive(:clean_vm).with(:hostname=>"vmx1",:template=>"ubuntuprecise")
+    @provisioning_service.should_receive(:clean_vm).with(:hostname => "vmx1", :template => "ubuntuprecise")
 
     @image_service.should_receive(:build_image).with("ubuntuprecise", network_config(network_address)).ordered
     @vm_service.should_receive(:define_vm).ordered
     @vm_service.should_receive(:start_vm).ordered
-    @provisioning_service.provision_vm(:hostname=>"vmx1",:template=>"ubuntuprecise")
+    @provisioning_service.provision_vm(:hostname => "vmx1", :template => "ubuntuprecise")
   end
 
   it 'allows the user to clean up vms' do
     @numbering_service.should_receive(:remove_ips_for)
     @vm_service.should_receive(:destroy_vm).ordered
     @vm_service.should_receive(:undefine_vm).ordered
-    @provisioning_service.clean_vm(:hostname=>"vmx1",:template=>"ubuntuprecise")
+    @provisioning_service.clean_vm(:hostname => "vmx1", :template => "ubuntuprecise")
   end
 
   it 'allows defaults to be passed to new MachineSpecs' do
@@ -88,32 +87,31 @@ describe Provision::Core::ProvisioningService do
       :vm_service => @vm_service,
       :numbering_service => @numbering_service,
       :defaults => {
-        :template=> "ubuntuprecise"
+        :template => "ubuntuprecise"
       }
     )
     @numbering_service.should_receive(:allocate_ips_for)
-    @provisioning_service.should_receive(:clean_vm).with(:hostname=>"vmx1",:template=>"ubuntuprecise")
-    @image_service.should_receive(:build_image).with("ubuntuprecise",anything).ordered
+    @provisioning_service.should_receive(:clean_vm).with(:hostname => "vmx1", :template => "ubuntuprecise")
+    @image_service.should_receive(:build_image).with("ubuntuprecise", anything).ordered
     @vm_service.should_receive(:define_vm).ordered
     @vm_service.should_receive(:start_vm).ordered
-    @provisioning_service.provision_vm(:hostname=>"vmx1")
-
+    @provisioning_service.provision_vm(:hostname => "vmx1")
   end
+
   it 'overrides defaults passed to new MachineSpecs' do
     @provisioning_service = Provision::Core::ProvisioningService.new(
       :image_service => @image_service,
       :vm_service => @vm_service,
       :numbering_service => @numbering_service,
       :defaults => {
-        :template=> "blah"
+        :template => "blah"
       }
     )
     @numbering_service.should_receive(:allocate_ips_for)
-    @provisioning_service.should_receive(:clean_vm).with(:hostname=>"vmx1",:template=>"ubuntuprecise")
-    @image_service.should_receive(:build_image).with("ubuntuprecise",anything).ordered
+    @provisioning_service.should_receive(:clean_vm).with(:hostname => "vmx1", :template => "ubuntuprecise")
+    @image_service.should_receive(:build_image).with("ubuntuprecise", anything).ordered
     @vm_service.should_receive(:define_vm).ordered
     @vm_service.should_receive(:start_vm).ordered
-    @provisioning_service.provision_vm(:hostname=>"vmx1", :template=>"ubuntuprecise")
-
+    @provisioning_service.provision_vm(:hostname => "vmx1", :template => "ubuntuprecise")
   end
 end
