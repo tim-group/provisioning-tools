b73e1d4cdbc672ed07923168965997c053238232
Tomas Doran <tomas.doran@timgroup.com>
2013-03-18 15:46:06 +0000

    Fix insane tests using relative paths

--- spec/provision/vm/virsh_spec.rb
+++ spec/provision/vm/virsh_spec.rb
@@ -3,12 +3,15 @@ require 'rspec'
 require 'provision'
 require 'provision/vm/virsh'
 require 'provision/core/machine_spec'
+require 'tmpdir'
 
 describe Provision::VM::Virsh do
   before do
   end
 
   it 'creates a virt machine xml file in libvirt' do
+    d = Dir.mktmpdir
+
     machine_spec = Provision::Core::MachineSpec.new(
       :hostname=>"vmx1",
       :disk_dir=>"build/",
@@ -16,15 +19,15 @@ describe Provision::VM::Virsh do
       :ram => "1G",
       :interfaces => [{:type=>"bridge",:name=>"br0"}, {:type=>"network", :name=>"provnat0"}],
       :images_dir => "build",
-      :libvirt_dir => "build"
+      :libvirt_dir => d
     )
 
     virt_manager = Provision::VM::Virsh.new()
     virt_manager.define_vm(machine_spec)
-    File.exist?("build/vmx1.xml").should eql(true)
-    
-    IO.read("build/vmx1.xml").should match("vmx1")
-    IO.read("build/vmx1.xml").should match("1G")
-    IO.read("build/vmx1.xml").should match("build/vmx1.img")
+    File.exist?("#{d}/vmx1.xml").should eql(true)
+
+    IO.read("#{d}/vmx1.xml").should match("vmx1")
+    IO.read("#{d}/vmx1.xml").should match("1G")
+    IO.read("#{d}/vmx1.xml").should match("build/vmx1.img")
   end
 end
