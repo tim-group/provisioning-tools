3473d3dd26f08be65a6fd6dfb164e26c1b59f2bc
Richard Pearce <richard.pearce@timgroup.com>
2013-04-19 16:29:44 +0100

    rpearce/mshah: Adding a sleep 30 = sad face, but needs to be done to ensure puppetdb is ready before re-runnning puppet. FIXME

--- home/image_builders/puppetmaster.rb
+++ home/image_builders/puppetmaster.rb
@@ -20,11 +20,16 @@ define 'puppetmaster' do
   run('deploy puppetmaster') {
     open("#{spec[:temp_dir]}/etc/rc.local", 'w') { |f|
 
-      f.puts "#!/bin/sh -e
+      f.puts "#!/bin/bash -e
 echo 'Run ntpdate'
 /usr/sbin/ntpdate -s dc-1.net.local
 echo 'Run puppet apply'
 /usr/bin/puppet apply --pluginsync --modulepath=/etc/puppet/modules --logdest=syslog /etc/puppet/manifests/site.pp
+
+sleep 30
+
+echo 'Run puppet again due to not loading facts and other stuff.. meh'
+/usr/bin/puppet agent -t
 echo \"#!/bin/sh -e\nexit 0\" > /etc/rc.local"
     }
   }
