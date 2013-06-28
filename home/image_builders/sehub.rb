b1ffa58dd1b25b2cd45e402976a2f40fcb1fc794
David Ellis <david.ellis@timgroup.com>
2013-06-28 12:35:27 +0100

    dellis: put together selenium builds

---
+++ home/image_builders/sehub.rb
@@ -0,0 +1,45 @@
+define "sehub" do
+  copyboot
+
+  run("run apt-update ") {
+    chroot "apt-get -y --force-yes update"
+  }
+
+  run("create ci user") {
+    chroot "/usr/sbin/useradd ci -M"
+  }
+  run("mounting devices") {
+    cmd "mount --bind /dev #{spec[:temp_dir]}/dev"
+    cmd "mount -t proc none #{spec[:temp_dir]}/proc"
+    cmd "mount -t sysfs none #{spec[:temp_dir]}/sys"
+  }
+
+  cleanup {
+    log.debug("cleaning up procs")
+    log.debug(`mount -l | grep #{spec[:hostname]}/proc | wc -l`.chomp)
+    keep_doing {
+      suppress_error.cmd "umount -l #{spec[:temp_dir]}/proc"
+    }.until {`mount -l | grep #{spec[:hostname]}/proc | wc -l`.chomp == "0"}
+
+    keep_doing {
+      suppress_error.cmd "umount -l #{spec[:temp_dir]}/sys"
+    }.until {`mount -l | grep #{spec[:hostname]}/sys | wc -l`.chomp == "0"}
+
+    keep_doing {
+      suppress_error.cmd "umount -l #{spec[:temp_dir]}/dev"
+    }.until {`mount -l | grep #{spec[:hostname]}/dev | wc -l`.chomp == "0"}
+  }
+
+
+  run("install selenium packages") {
+    apt_install "openjdk-6-jdk"
+    apt_install "selenium"
+    apt_install "selenium-hub"
+#    chroot "update-rc.d selenium-node defaults"
+ #   chroot "sed -i'.bak' -e 's#^securerandom.source=file:/dev/urandom#securerandom.source=file:/dev/../dev/urandom#g' /etc/java-6-openjdk/security/java.security"
+  }
+
+  cleanup {
+    chroot "/etc/init.d/selenium-hub stop"
+  }
+end
