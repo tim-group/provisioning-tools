54e3855a279021a068f3994b2db5f7ea14a4c625
Tom Denley <tom.denley@youdevise.com>
2013-09-13 17:02:46 +0100

    tomd: make script resilient in the face of cwd

--- bin/launch_local_puppetmaster.sh
+++ bin/launch_local_puppetmaster.sh
@@ -6,17 +6,18 @@ if (( EUID != 0 )); then
    exit 100
 fi
 
-env="${1:-${env:-dev}}"
+BIN="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ENVIRONMENT="${1:-${env:-dev}}"
 
 rake network
-bin/dns deallocate "${env}-puppetmaster-001.mgmt.dev.net.local"
-bin/dns allocate "${env}-puppetmaster-001.mgmt.dev.net.local"
+"${BIN}"/dns deallocate "${ENVIRONMENT}-puppetmaster-001.mgmt.dev.net.local"
+"${BIN}"/dns allocate "${ENVIRONMENT}-puppetmaster-001.mgmt.dev.net.local"
 sed -i 's/^\(.*puppet.mgmt.dev.net.local.*\)$/#\1/' /etc/hosts
-sed -i "s/${env}-puppetmaster-001.mgmt.dev.net.local/${env}-puppetmaster-001.mgmt.dev.net.local puppet.mgmt.dev.net.local/g" /etc/hosts
+sed -i "s/${ENVIRONMENT}-puppetmaster-001.mgmt.dev.net.local/${ENVIRONMENT}-puppetmaster-001.mgmt.dev.net.local puppet.mgmt.dev.net.local/g" /etc/hosts
 rake network
-virsh destroy "${env}-puppetmaster-001"
-virsh undefine "${env}-puppetmaster-001"
-cat files/puppetmaster.yaml | sed "s/dev-puppetmaster-001/${env}-puppetmaster-001/g" | bin/provision
+virsh destroy "${ENVIRONMENT}-puppetmaster-001"
+virsh undefine "${ENVIRONMENT}-puppetmaster-001"
+cat files/puppetmaster.yaml | sed "s/dev-puppetmaster-001/${ENVIRONMENT}-puppetmaster-001/g" | "${BIN}"/provision
 
 #wait for puppetmaster: to come up
 #launch ref apps
