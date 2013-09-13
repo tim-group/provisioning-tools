b34ac1e841f085c54f6ac2a0a09b06378beaeff0
Tom Denley <tom.denley@youdevise.com>
2013-09-13 16:01:55 +0100

    tomd/gcarlyle: rather than preceding every line with sudo, just ask the user to run this script as root

--- bin/launch_local_puppetmaster.sh
+++ bin/launch_local_puppetmaster.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-set -e
 set -u
+
+if (( EUID != 0 )); then
+   echo "You sudo this." 1>&2
+   exit 100
+fi
 
 env="${1:-${env:-dev}}"
 
-sudo rake network
-sudo bin/dns deallocate "${env}-puppetmaster-001.mgmt.dev.net.local"
-sudo bin/dns allocate "${env}-puppetmaster-001.mgmt.dev.net.local"
-sudo sed -i "s/dev-puppetmaster-001.mgmt.dev.net.local/${env}-puppetmaster-001.mgmt.dev.net.local puppet.mgmt.dev.net.local/g" /etc/hosts
-sudo rake network
-sudo virsh destroy "${env}-puppetmaster-001"
-sudo virsh undefine "${env}-puppetmaster-001"
-cat files/puppetmaster.yaml | sed "s/dev-puppetmaster-001/${env}-puppetmaster-001/g" | sudo bin/provision
+rake network
+bin/dns deallocate "${env}-puppetmaster-001.mgmt.dev.net.local"
+bin/dns allocate "${env}-puppetmaster-001.mgmt.dev.net.local"
+sed -i "s/dev-puppetmaster-001.mgmt.dev.net.local/${env}-puppetmaster-001.mgmt.dev.net.local puppet.mgmt.dev.net.local/g" /etc/hosts
+rake network
+virsh destroy "${env}-puppetmaster-001"
+virsh undefine "${env}-puppetmaster-001"
+cat files/puppetmaster.yaml | sed "s/dev-puppetmaster-001/${env}-puppetmaster-001/g" | bin/provision
 
 #wait for puppetmaster: to come up
 #launch ref apps
