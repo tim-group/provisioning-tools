7b401ad9a306d2326a02ffa3d3e2ad26fbdcaed4
Adam Gibbins <adam@adamgibbins.com>
2014-03-11 20:51:22 +0000

    Fix hardcoded references to jave 6

--- home/image_builders/senode.rb
+++ home/image_builders/senode.rb
@@ -48,7 +48,7 @@ define "senode" do
     apt_install "selenium"
     apt_install "selenium-node"
     chroot "update-rc.d selenium-node defaults"
-    chroot "sed -i'.bak' -e 's#^securerandom.source=file:/dev/urandom#securerandom.source=file:/dev/../dev/urandom#g' /etc/java-6-openjdk/security/java.security"
+    chroot "sed -i'.bak' -e 's#^securerandom.source=file:/dev/urandom#securerandom.source=file:/dev/../dev/urandom#g' /etc/java-7-openjdk/security/java.security"
     chroot "ln -s /usr/lib/firefox/firefox /usr/bin/firefox-bin"
   }
 
