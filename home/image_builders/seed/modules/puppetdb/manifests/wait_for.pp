2bb050f81c111a7853b068d35cc3b2f8c097ee2c
Adam Gibbins <adam@adamgibbins.com>
2014-08-27 16:38:16 +0100

    Wait for the port that we're actually using

--- home/image_builders/seed/modules/puppetdb/manifests/wait_for.pp
+++ home/image_builders/seed/modules/puppetdb/manifests/wait_for.pp
@@ -1,7 +1,7 @@
 class puppetdb::wait_for($puppetmaster = "localhost") {
   exec { 'waitfor_puppetdb':
     cwd         => '/tmp',
-    command     => "curl -H 'Accept: application/json' 'http://${puppetmaster}:8080/metrics/mbean/com.puppetlabs.puppetdb.query.population:type=default,name=num-resources'",
+    command     => "curl -H 'Accept: application/json' 'http://${puppetmaster}:8081/metrics/mbean/com.puppetlabs.puppetdb.query.population:type=default,name=num-resources'",
     try_sleep   => 1,
     tries       => 60,
     refreshonly => true,
