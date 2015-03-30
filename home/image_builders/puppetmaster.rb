5b39fc5e356782f2622879114f2b6f4023cb81b2
Michal Mazurek <michal.mazurek@timgroup.com>
2015-03-30 17:16:53 +0100

    debug puppetmaster

--- home/image_builders/puppetmaster.rb
+++ home/image_builders/puppetmaster.rb
@@ -35,10 +35,10 @@ define 'puppetmaster' do
              "mkdir -p /etc/puppet/environments/masterbranch\n" \
              "git clone http://git.youdevise.com/git/puppet /etc/puppet/environments/masterbranch\n" \
              "echo 'Run puppet apply' | logger\n" \
-             "puppet apply --debug --verbose --pluginsync --modulepath=/etc/puppet/modules --logdest=syslog /etc/puppet/manifests\n" \
-             "/etc/init.d/apache2-puppetmaster restart 2>&1 | logger\n" \
-             "puppet agent --debug --verbose --waitforcert 10 --onetime 2>&1 | logger\n" \
-             "sleep 1 ; puppet cert sign $(hostname -f) 2>&1 | logger\n" \
+            # "puppet apply --debug --verbose --pluginsync --modulepath=/etc/puppet/modules --logdest=syslog /etc/puppet/manifests\n" \
+            # "/etc/init.d/apache2-puppetmaster restart 2>&1 | logger\n" \
+            # "puppet agent --debug --verbose --waitforcert 10 --onetime 2>&1 | logger\n" \
+            # "sleep 1 ; puppet cert sign $(hostname -f) 2>&1 | logger\n" \
              "echo \"#!/bin/sh -e\n\nexit 0\" > /etc/rc.local\n"
     }
   }
