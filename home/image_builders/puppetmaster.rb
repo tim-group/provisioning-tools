aa139b22a5840c256909fefaebe94396d9c089a4
Michal Mazurek <michal.mazurek@timgroup.com>
2015-04-02 08:15:40 +0100

    one less git clone for the puppetmaster

--- home/image_builders/puppetmaster.rb
+++ home/image_builders/puppetmaster.rb
@@ -25,20 +25,24 @@ define 'puppetmaster' do
     open("#{spec[:temp_dir]}/etc/rc.local", 'w') { |f|
       f.puts "#!/bin/bash\n" \
              "\n" \
-             "echo 'Run ntpdate' | logger\n" \
+             "echo 'running ntpdate...' | logger\n" \
              "/etc/init.d/ntp stop | logger 2>&1\n" \
              "/usr/sbin/ntpdate -s ci-1.youdevise.com | logger 2>&1\n" \
              "/etc/init.d/ntp start | logger 2>&1\n" \
-             "echo 'Clone the masterbranch puppet branch' | logger\n" \
-             "git clone --mirror git://git.youdevise.com/puppet.git /etc/puppet/puppet.git\n" \
-             "mkdir -p /etc/puppet/environments\n" \
-             "mkdir -p /etc/puppet/environments/masterbranch\n" \
-             "git clone git://git.youdevise.com/puppet.git /etc/puppet/environments/masterbranch\n" \
-             "echo 'Run puppet apply' | logger\n" \
-             "puppet apply --debug --verbose --pluginsync --modulepath=/etc/puppet/modules --logdest=syslog /etc/puppet/manifests\n" \
+             "\n" \
+             "echo 'cloning the masterbranch puppet branch...' | logger\n" \
+             "mkdir -p /etc/puppet/environments/masterbranch/\n" \
+             "git clone git://git.youdevise.com/puppet.git /etc/puppet/environments/masterbranch/\n" \
+             "\n" \
+             "echo 'running puppet apply...' | logger\n" \
+             "puppet apply --debug --verbose --pluginsync --modulepath=/etc/puppet/environments/masterbranch/modules --logdest=syslog /etc/puppet/environments/masterbranch/manifests\n" \
              "/etc/init.d/apache2-puppetmaster restart 2>&1 | logger\n" \
+             "\n" \
+             "echo 'running puppet agent...' | logger\n" \
              "puppet agent --debug --verbose --waitforcert 10 --onetime 2>&1 | logger\n" \
              "sleep 10 ; puppet cert sign $(hostname -f) 2>&1 | logger\n" \
+             "\n" \
+             "echo 'all done' | logger\n" \
              "echo \"#!/bin/sh -e\n\nexit 0\" > /etc/rc.local\n"
     }
   }
