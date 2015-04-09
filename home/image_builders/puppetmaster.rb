42f5e3da6993f33b414423765d7bb2f494d7793e
Michal Mazurek <michal.mazurek@timgroup.com>
2015-04-09 08:34:31 +0100

    puppetmaster.rb
    symlink hieradata, remove it after puppet apply runs
    more verbose comment
    break long lines

--- home/image_builders/puppetmaster.rb
+++ home/image_builders/puppetmaster.rb
@@ -14,7 +14,9 @@ define 'puppetmaster' do
 
   run('deploy puppetmaster') {
     # * sets up puppet.git and environments/masterbranch for puppetupdate
-    # * the puppetmaster needs environments/masterbranch to bootstrap
+    # * runs puppet apply from the checked out masterbranch
+    # * runs a proper puppet agent, signs the cert for itself
+    # * XXX symlink hieradata for the puppet apply run, a better way would be to specify hieradata path as an argument
     # XXX dev-puppetmaster currently broken, clone into /etc/puppet for it to work
     open("#{spec[:temp_dir]}/etc/rc.local", 'w') { |f|
       f.puts "#!/bin/bash\n" \
@@ -28,13 +30,20 @@ define 'puppetmaster' do
              "git clone --mirror git://git.youdevise.com/puppet.git /etc/puppet/puppet.git\n" \
              "mkdir -p /etc/puppet/environments/masterbranch/\n" \
              "echo 'checking out the master branch...' | logger\n" \
-             "git --git-dir=/etc/puppet/puppet.git --work-tree=/etc/puppet/environments/masterbranch/ checkout --detach --force master\n" \
-             "ln -s /etc/puppet/environments/masterbranch/modules/puppetmaster/files/hiera.yaml /etc/puppet/hiera.yaml\n" \
-             "ln -s /etc/puppet/environments/masterbranch/modules/puppetmaster/files/auth.conf /etc/puppet/auth.conf\n" \
-             "ln -s /etc/puppet/environments/masterbranch/modules/puppetmaster/files/routes.yaml /etc/puppet/routes.yaml\n" \
+             "git --git-dir=/etc/puppet/puppet.git --work-tree=/etc/puppet/environments/masterbranch/ checkout " \
+               "--detach --force master\n" \
+             "ln -s /etc/puppet/environments/masterbranch/modules/puppetmaster/files/hiera.yaml " \
+               "/etc/puppet/hiera.yaml\n" \
+             "ln -s /etc/puppet/environments/masterbranch/modules/puppetmaster/files/auth.conf " \
+               "/etc/puppet/auth.conf\n" \
+             "ln -s /etc/puppet/environments/masterbranch/modules/puppetmaster/files/routes.yaml " \
+               "/etc/puppet/routes.yaml\n" \
+             "ln -s /etc/puppet/environments/masterbranch/hieradata/ hieradata # XXX needed for puppet apply\n" \
              "\n" \
              "echo 'running puppet apply...' | logger\n" \
-             "puppet apply --debug --verbose --pluginsync --modulepath=/etc/puppet/environments/masterbranch/modules --logdest=syslog /etc/puppet/environments/masterbranch/manifests\n" \
+             "puppet apply --debug --verbose --pluginsync --modulepath=/etc/puppet/environments/masterbranch/modules" \
+               "--logdest=syslog /etc/puppet/environments/masterbranch/manifests\n" \
+             "rm /etc/puppet/hieradata # XXX no longer needed\n" \
              "/etc/init.d/apache2-puppetmaster restart 2>&1 | logger\n" \
              "\n" \
              "echo 'running puppet agent...' | logger\n" \
