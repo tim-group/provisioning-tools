3ad33ff21dd17e3294b45043fc74e73f8ee660fa
Michal Mazurek <michal.mazurek@timgroup.com>
2015-07-30 11:14:24 +0100

    on second thought, don't drop support for previous version yet

--- mcollective/agent/computenode/actions.rb
+++ mcollective/agent/computenode/actions.rb
@@ -2,6 +2,7 @@
 
 require 'json'
 
+$: << '/opt/provisioning-tools/lib/ruby/site_ruby'
 $: << '/usr/local/lib/site_ruby/timgroup/'
 require 'provisioning-tools/provision'
 require 'provisioning-tools/provision/workqueue'
