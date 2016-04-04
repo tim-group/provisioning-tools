3d668591102b5d9a27f429bf2d500961e905933d
Gary Richards <gary.richards@youdevise.com>
2016-04-04 16:22:38 +0100

    Whilst we have the EOF issue, disable http keepalive in an attempt to get things rebuilding ok

--- lib/provisioning-tools/home/image_builders/puppetclient.rb
+++ lib/provisioning-tools/home/image_builders/puppetclient.rb
@@ -18,7 +18,8 @@ define "puppetclient" do
         "  srv_domain                     = #{spec[:logicalenv]}.mgmt.#{spec[:domain]}\n" \
         "  # BUG Fixed in Puppet 4.0 (https://tickets.puppetlabs.com/browse/PUP-1035)\n" \
         "  pluginsource                   = puppet:///plugins\n" \
-        "  pluginfactsource               = puppet:///pluginfacts\n"
+        "  pluginfactsource               = puppet:///pluginfacts\n" \
+        "  http_keepalive_timeout         = -1\n"
     end
   end
 
