6ca6da28e3b3b0c0c9c5eadb762c4982180921c5
Richard Pijnenburg <richard.pijnenburg@timgroup.com>
2016-04-08 17:48:05 +0100

    Revert changes here. FIxed it in puppet

--- lib/provisioning-tools/home/image_builders/puppetclient.rb
+++ lib/provisioning-tools/home/image_builders/puppetclient.rb
@@ -38,15 +38,13 @@ define "puppetclient" do
     open("#{spec[:temp_dir]}/etc/rc.local", 'w') do |f|
       f.puts "#!/bin/sh -e\n" \
         "echo 'Running rc.local' | logger\n" \
-        "echo 'Run ntpdate' | logger\n" \
+        "echo 'Run ntpdate'\n" \
         "(/usr/sbin/ntpdate -b -v -d -s ci-1.youdevise.com 2>&1 | tee -a /tmp/bootstrap.log || exit 0)\n" \
-        "echo 'Regenerating SSH hostkeys' | logger\n" \
+        "echo 'Regenerating SSH hostkeys'\n" \
         "/bin/rm /etc/ssh/ssh_host_*\n" \
         "/usr/sbin/dpkg-reconfigure openssh-server\n" \
-        "echo 'Running puppet agent' | logger\n" \
+        "echo 'Running puppet agent'\n" \
         "puppet agent --debug --verbose --waitforcert 10 --onetime 2>&1 | tee -a /tmp/bootstrap.log\n" \
-        "echo 'creating initial facts' | logger\n" \
-        "/usr/local/sbin/refresh-mcollective-metadata\n" \
         "echo \"#!/bin/sh -e\\nexit 0\" > /etc/rc.local\n" \
         "echo 'Finished running rc.local'\n" \
         "exit 0\n"
