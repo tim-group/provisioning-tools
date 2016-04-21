f72fd4eca6b5a84918ae5c4dede2f0bc9cc021d5
Gary Richards <gary.richards@youdevise.com>
2016-04-21 10:40:25 +0100

    Remove all of my debugging

--- lib/provisioning-tools/home/image_builders/puppetclient.rb
+++ lib/provisioning-tools/home/image_builders/puppetclient.rb
@@ -41,19 +41,12 @@ define "puppetclient" do
         "echo 'Regenerating SSH hostkeys'\n" \
         "/bin/rm /etc/ssh/ssh_host_*\n" \
         "/usr/sbin/dpkg-reconfigure openssh-server\n" \
-        "tcpdump -i any -e -n -s0 port 8140 -w /tmp/out.pcap &" \
         "echo 'Running puppet agent'\n" \
         "puppet agent --debug --verbose --waitforcert 10 --onetime 2>&1 | tee -a /tmp/bootstrap.log\n" \
-        "killall tcpdump" \
         "echo \"#!/bin/sh -e\\nexit 0\" > /etc/rc.local\n" \
         "echo 'Finished running rc.local'\n" \
         "exit 0\n"
     end
   end
 
-  run("hack some debugging into the gold image") do
-    cmd("cp " \
-        "/usr/local/lib/site_ruby/timgroup/provisioning-tools/home/puppet_network_http_pool.rb " \
-        "#{spec[:temp_dir]}/usr/lib/ruby/vendor_ruby/puppet/network/http/pool.rb")
-  end
 end
